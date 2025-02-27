9a879f96709192f6849af9eb4087223b01a8841f
Martin Faure <127661359+martinfaure@users.noreply.github.com>
2025-02-27 21:31:55 +0100

    changes-check-winner-function

--- app.js
+++ app.js
@@ -220,10 +220,64 @@ const grille = [
 ];
 
 // ticket 24
-function checkWinner() {
+
+const grilleAvecGagnant1 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["X", "X", "X", "X", "", "", ""],
+];
+
+const grilleAvecGagnant2 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+];
+
+const grilleAvecGagnant3 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "X", "", "", ""],
+	["", "", "X", "", "", "", ""],
+	["", "X", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+];
+
+const grilleAvecGagnant4 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "X", "", "", ""],
+	["", "", "X", "O", "", "", ""],
+	["", "O", "O", "O", "", "", ""],
+	["X", "O", "O", "O", "O", "", ""],
+];
+
+const grilleSansGagnant = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "X", "", "", ""],
+	["", "", "X", "O", "", "", ""],
+	["", "O", "O", "O", "", "", ""],
+	["X", "O", "O", "X", "O", "", ""],
+];
+
+function checkWinner(grille) {
 	const rows = 6;
 	const columns = 7;
 	const board = [];
 
 	const buttons = document.querySelectorAll(".grid-game .grid");
 }
+
+let resultat = "";
+
+resultat = checkWinner(grilleAvecGagnant1); // retourne "X"
+resultat = checkWinner(grilleAvecGagnant2); // retourne "X"
+resultat = checkWinner(grilleAvecGagnant3); // retourne "X"
+resultat = checkWinner(grilleAvecGagnant4); // retourne "O"
+resultat = checkWinner(grilleSansGagnant); // retourne ""
